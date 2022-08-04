bba26d1fc7b83f119a465562dc1cb813109847b7
Adrian F. Cobena <adrianfcobena@gmail.com>
2022-08-04 02:44:08 +0200

    preparar perfil user

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -12,42 +12,7 @@ router.get("/", isLoggedIn, (req, res, next) => {
 })
 
 
-router.get("/:userId", async (req, res, next) => {
-  try {
-    const {newId} = req.params
-    const detailId = await New.findById(newId).populate('owner')
-    console.log(detailId)
-    const comment = await Comment.find({news: newId}).populate('owner');
-    console.log("this is the", comment)
-
-    /* res.render("news/details.hbs", {detailId, comment}) */
-    /* const myOwner = await New.findById(newId).populate("owner"); */
-
-    let isOwner = false;
-    if (req.session.user !== undefined) {
-      if (req.session.user._id == detailId.owner._id) {
-        console.log("le llamaban req", req.session.user._id)
-        isOwner = true;
-      } else {
-        isOwner = false;
-      }
-    res.render("news/details.hbs", {
-
-      detailId,
-      isOwner,
-      comment
-      /* myOwner */
-    });
-  } else {
-    res.render("news/details.hbs", {
-      detailId,
-      comment,
-    });
-  }
-  } catch (err) {
-    next(err)
-  }
-});
+/*  */
 
 
 
