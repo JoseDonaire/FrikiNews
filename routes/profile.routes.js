bec71124b643dc32b9551c8da6bf6bfd5bd494f9
Adrian F. Cobena <adrianfcobena@gmail.com>
2022-08-04 11:10:30 +0200

    owner and isUserAdmin

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -12,9 +12,40 @@ router.get("/", isLoggedIn, (req, res, next) => {
 })
 
 
-router.post('/')
+
+router.get("/:userId/details", async (req, res, next) => {
+  try {
+    const {userId} = req.params
+    const newsDetailId = await User.findById(userId).populate('owner')
+    /* console.log(newsDetailId) */
 
 
+    /* res.render("news/details.hbs", {newsDetailId, comment}) */
+    /* const myOwner = await New.findById(newId).populate("owner"); */
+
+    let isOwner = false;
+    if (req.session.user !== undefined) {
+      if (req.session.user._id == newsDetailId.owner._id) {
+        /* console.log("le llamaban req", req.session.user._id) */
+        isOwner = true;
+      } else {
+        isOwner = false;
+      }
+    res.render("news/details.hbs", {
+
+      newsDetailId,
+      isOwner,
+      /* myOwner */
+    });
+  } else {
+    res.render("news/details.hbs", {
+      newsDetailId,
+    });
+  }
+  } catch (err) {
+    next(err)
+  }
+});
 
 
 
